c64a76233d7a57168ae1503d1acec858fa7a1b5d
matswuuu <115926657+matswuuu@users.noreply.github.com>
2023-06-26 18:09:30 +1000

    Update clicker.sh

--- clicker.sh
+++ clicker.sh
@@ -3,7 +3,6 @@ while :
 do
     for w in $windows; do
         xdotool click --window $w 1
-        xdotool key --window $w t slash s e l l space a l l
-
+        xdotool key --window $w t slash s e l l space a l l enter
     done
 done

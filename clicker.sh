1fba16ab54666460a57b57cce43fa7f0d6f9c279
matswuuu <115926657+matswuuu@users.noreply.github.com>
2023-06-26 18:05:45 +1000

    Add files via upload

---
+++ clicker.sh
@@ -0,0 +1,9 @@
+windows=$(xdotool search --name cristalix)
+while :
+do
+    for w in $windows; do
+        xdotool click --window $w 1
+        xdotool key --window $w t slash s e l l space a l l
+
+    done
+done

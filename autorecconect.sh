adb13e1efd2bcaf1db13412427e79f2dcc16dfcc
matswuuu <115926657+matswuuu@users.noreply.github.com>
2023-07-17 13:51:44 +1000

    Update autorecconect.sh

--- autorecconect.sh
+++ autorecconect.sh
@@ -4,8 +4,10 @@ while :
 do
     for w in $windows; do
         for ((i=1;i<=2;i++)); do
-          xdotool key --window $w h c minus j o i n minus t o space s b Return 
-          xdotool sleep 5 key --window $w $slot 
+          xdotool windowactivate $w
+        
+          xdotool key --window $w --delay 1000 slash h c minus j o i n minus t o space s b Return 
+          xdotool sleep 10 key --window $w $slot 
           xdotool click --window $w 3
           
           sleep 5

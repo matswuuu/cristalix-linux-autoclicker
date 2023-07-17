58d1b3bd84cee679df8ca8b8abf587612687835e
matswuuu <115926657+matswuuu@users.noreply.github.com>
2023-07-17 13:24:47 +1000

    Update autorecconect.sh

--- autorecconect.sh
+++ autorecconect.sh
@@ -1,1 +1,13 @@
-
+slot=5
+windows=$(xdotool search --name cristalix)
+while :
+do
+    for ((i=1;i<=2;i++)); do
+      xdotool key --window $w h c minus j o i n minus t o space s b Return 
+      xdotool sleep 5 key --window $w $slot 
+      xdotool click --window $w 3
+      
+      sleep 5
+    done
+    sleep 1800
+done

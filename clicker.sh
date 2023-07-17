d320073b19e02208d5b53b5763c3e8716f24aac6
matswuuu <115926657+matswuuu@users.noreply.github.com>
2023-07-17 13:27:57 +1000

    Update clicker.sh

--- clicker.sh
+++ clicker.sh
@@ -1,14 +1,9 @@
-slot=7
 windows=$(xdotool search --name cristalix)
 while :
 do
     for w in $windows; do
         for ((i=1;i<=10;i++)); do
-            xdotool click --window $w 1
-            sleep 0.15
-        done
-
-        xdotool key --window $w $slot
-        xdotool click --window $w 3
+        xdotool click --window $w 1
+        sleep 0.15
     done
 done
